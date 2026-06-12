a7a54bc2e7f39c704fdb26f2270ebe98f603be93
nemesisneon15 <n3m3slsne0n@gmail.com>
2026-06-12 11:38:20 +0200

    Add Pokemon dialog facts and navigation

--- assets/scripts/templates.js
+++ assets/scripts/templates.js
@@ -35,6 +35,28 @@ function getDialogStat(item) {
 }
 
 
+function getDialogFacts(pokemon) {
+  const ability = pokemon.abilities[0].ability.name;
+  return `
+    <dl class="dialog-card__facts">
+      <div><dt>Height</dt><dd>${pokemon.height / 10} m</dd></div>
+      <div><dt>Weight</dt><dd>${pokemon.weight / 10} kg</dd></div>
+      <div><dt>Ability</dt><dd>${ability}</dd></div>
+    </dl>
+  `;
+}
+
+
+function getDialogNavigation() {
+  return `
+    <nav class="dialog-card__navigation" aria-label="Pokémon navigation">
+      <button data-id="prev-button" type="button" disabled>← Previous</button>
+      <button data-id="next-button" type="button" disabled>Next →</button>
+    </nav>
+  `;
+}
+
+
 function getDialogContent(pokemon) {
   const types = pokemon.types.map((item) => item.type.name);
   const image = pokemon.sprites.other["official-artwork"].front_default;
@@ -44,5 +66,7 @@ function getDialogContent(pokemon) {
     <div class="dialog-card__types">${getTypeBadges(types)}</div>
     <img data-id="dialog-image" src="${image}" alt="${pokemon.name}">
     <ul class="dialog-card__stats">${getDialogStats(pokemon.stats)}</ul>
+    ${getDialogFacts(pokemon)}
+    ${getDialogNavigation()}
   `;
 }
